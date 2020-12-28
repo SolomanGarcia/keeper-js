3bdd7e0019d0716fde954b2ab604ed3b86c8b3ae
Soloman Garcia <soloman.garcia@gmail.com>
2020-12-28 09:35:02 -0700

    work on openFrom method

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 class App {
   constructor() {
     this.$form = document.querySelector("#form");
+    this.$noteTitle = document.querySelector("#note-title");
+    this.$formButtons = document.querySelector("#form-buttons");
+
     this.addEventListeners();
   }
 
@@ -12,6 +15,18 @@ class App {
 
   handleFormClick(event) {
     const isFormClicked = this.$form.contains(event.target);
+
+    if (isFormClicked) {
+      // open form
+    } else {
+      // close form
+    }
+  }
+
+  openForm() {
+    this.$form.classList.add("form-open");
+    this.$noteTitle.style.display = "block";
+    this.$formButtons.style.display = "block";
   }
 }
 

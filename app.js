29e1635844945da19e01ce789c8c5d3cf868c999
Soloman Garcia <soloman.garcia@gmail.com>
2020-12-29 08:10:10 -0700

    refactor

--- app.js
+++ app.js
@@ -32,9 +32,15 @@ class App {
   handleFormClick(event) {
     const isFormClicked = this.$form.contains(event.target);
 
+    const title = this.$noteTitle.value;
+    const text = this.$noteText.value;
+    const hasNote = title || text;
+
     if (isFormClicked) {
       // open form
       this.openForm();
+    } else if (hasNote) {
+      this.addNote({ title, text });
     } else {
       // close form
       this.closeFrom();
@@ -51,25 +57,29 @@ class App {
     this.$form.classList.remove("form-open");
     this.$noteTitle.style.display = "none";
     this.$formButtons.style.display = "none";
+    this.$noteTitle.value = "";
+    this.$noteText.value = "";
   }
 
-  addNote(note) {
+  addNote({ title, text }) {
     const newNote = {
-      title: note.title,
-      text: note.text,
+      title,
+      text,
       color: "white",
       id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1
     };
     this.notes = [...this.notes, newNote];
     this.displayNotes();
+    this.closeFrom();
   }
 
   displayNotes() {
     const hasNotes = this.notes.length > 0;
     this.$placeholder.style.display = hasNotes ? "none" : "flex";
 
-    this.$notes.innerHTML = this.notes.map(
-      (note) => `
+    this.$notes.innerHTML = this.notes
+      .map(
+        (note) => `
     <div style="background: ${note.color};" class="note">
     <div class="${note.title && "note-title"}">${note.title}</div>
     <div class="note-text">${note.text}</div>
@@ -81,7 +91,8 @@ class App {
     </div>
   </div>
     `
-    );
+      )
+      .join("");
   }
 }
 
